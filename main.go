9907a192459ac18afa189c6433843c756d422b67
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2025-02-09 14:39:28 -0300

    substitui a estrutura do jogo pela nova implementação do pacote game

--- main.go
+++ main.go
@@ -1,30 +1,13 @@
 package main
 
 import (
+	"github.com/WilliamJonathan/gogame/game"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type Game struct {
-}
-
-// RESPONSAVEL POR ATUALIZAR A LOGICA DO JOGO
-func (g *Game) Update() error {
-	return nil
-}
-
-// RESPONSAVEL POR DESENHAR OBJETOS NA TELA
-func (g *Game) Draw(screen *ebiten.Image) {
-
-}
-
-// RESPONSAVEL POR RETORNAR O TAMANHO DA TELA
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return outsideWidth, outsideHeight
-}
-
 func main() {
 
-	g := &Game{}
+	g := game.NewGame()
 
 	err := ebiten.RunGame(g)
 	if err != nil {
