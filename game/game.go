6ea1f19ab330f8d828fcce4622a0e557ca22b8cf
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2025-02-09 14:39:22 -0300

    adiciona estrutura e lógica básica do jogo

---
+++ game/game.go
@@ -0,0 +1,31 @@
+package game
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct {
+	player *Player
+}
+
+func NewGame() *Game {
+	player := NewPlayer()
+	return &Game{
+		player: player,
+	}
+}
+
+// RESPONSAVEL POR ATUALIZAR A LOGICA DO JOGO
+func (g *Game) Update() error {
+	return nil
+}
+
+// RESPONSAVEL POR DESENHAR OBJETOS NA TELA
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.player.Draw(screen)
+}
+
+// RESPONSAVEL POR RETORNAR O TAMANHO DA TELA
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
